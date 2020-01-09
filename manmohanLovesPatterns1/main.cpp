c3b3ae3df15125b915a98f376917b882c70c777b
Karandeep Singh <greatprincekarandeep@gmail.com>
2020-01-09 21:15:33 +0530

    changes updated

--- manmohanLovesPatterns1/main.cpp
+++ manmohanLovesPatterns1/main.cpp
@@ -4,6 +4,29 @@ using namespace std;
 
 int main()
 {
-    cout << "Hello world!" << endl;
+    int n;
+    cin>>n;
+    for(int i=1;i<=n;i++){
+            if(i>1)
+                {cout<<1;}
+        if(i%2==0){
+            int num=i-2;
+            while(num>0){
+                cout<<0;
+                num--;
+            }
+
+        }
+        else{
+            int num2=i-2;
+            while(num2>0){
+                cout<<1;
+                num2--;
+            }
+
+        }
+        cout<<1<<endl;
+        }
+
     return 0;
 }
