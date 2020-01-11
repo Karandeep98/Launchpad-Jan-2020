c2bf7710e2e46f1e05b9f2dbc119c2947cde9163
Karandeep Singh <greatprincekarandeep@gmail.com>
2020-01-11 17:35:34 +0530

    updated codes

--- hollowRhombusPattern/main.cpp
+++ hollowRhombusPattern/main.cpp
@@ -4,6 +4,28 @@ using namespace std;
 
 int main()
 {
-    cout << "Hello world!" << endl;
+    int n;
+   cin>>n;
+   char c[100][100];
+   for(int i=0;i<n;i++){
+        for(int j=0;j<n;j++){
+            c[0][j]='*';
+            c[n-1][j]='*';
+            c[j][0]='*';
+            c[j][n-1]='*';
+        }
+   }
+   for(int i=0;i<n;i++){
+        int spaces=n-i-1;
+   while(spaces>0){
+    cout<<" ";
+    spaces--;
+   }
+    for(int j=0;j<n;j++){
+        cout<<c[i][j];
+    }
+    cout<<endl;
+   }
+
     return 0;
 }
