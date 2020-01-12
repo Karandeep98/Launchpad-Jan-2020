d234b1115939318e4938f452c1501f8ec653d1f3
Karandeep Singh <greatprincekarandeep@gmail.com>
2020-01-12 17:58:36 +0530

    adding new codes

--- ultraFastMathematician/main.cpp
+++ ultraFastMathematician/main.cpp
@@ -1,9 +1,24 @@
 #include <iostream>
+#include<string.h>
 
 using namespace std;
 
 int main()
 {
-    cout << "Hello world!" << endl;
+    int n;
+    char a[100],b[100];
+    cin>>n;
+    for(int i=0;i<n;i++){
+        cin>>a>>b;
+
+    for(int j=0;j<strlen(a);j++){
+        if(a[j]==b[j])
+            a[j]=0+'0';
+        else
+            a[j]=1+'0';
+
+    }
+    cout<<a<<endl;
+    }
     return 0;
 }
